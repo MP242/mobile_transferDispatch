d0c476ea491417f257888a7e6bac1c81c8b1d1e2
MP242 <marc.pollet@epitech.eu>
2023-03-13 09:43:57 +0400

    add loading state

--- App.tsx
+++ App.tsx
@@ -4,49 +4,16 @@ import HomeScreen from './screens/Home/Home';
 import AccountScreen from './screens/Account/Account';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/Feather';
 
 const Tab = createBottomTabNavigator();
-
-function HomeScreen2() {
-  return (
-    <SafeAreaView>
-      <StatusBar translucent={true} barStyle='light-content' backgroundColor="none"/>
-      <Text>Home Screen</Text>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        
-        <Text>Home Screen</Text>
-      </View>
-    </SafeAreaView>
-  );
-}
-
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
 
   return (
     <NavigationContainer>
-      {/* <Tab.Navigator  
-      screenOptions={{
-        headerTransparent: true,
-        headerTitleStyle: {color: 'white'},
-        headerTintColor: 'white',
-        tabBarStyle:{
-          backgroundColor: 'transparent',
-          borderTopWidth: 1,
-          borderTopColor: 'white',
-          position: 'absolute',
-          height: 100          
-        },
-        tabBarItemStyle:{
-          backgroundColor:'transparent',
-          margin:9,
-          borderRadius:10,
-        }
-      }}> */}
       <Tab.Navigator
       screenOptions={({ route }) => ({
         // headerTransparent: true,
@@ -83,8 +50,7 @@ function App(): JSX.Element {
                 default:
                     break;
             }
-            // return <Ionicons name={iconName} size={size} color={color} />;
-            // return <LottieView source={filePath} loop={false} autoPlay={focused} />;
+            
             return <Icon name={iconName} size={size} color={color} />;
         },
     })}
@@ -92,26 +58,12 @@ function App(): JSX.Element {
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Account" component={AccountScreen} />
       </Tab.Navigator> 
-    </NavigationContainer>
-    // <NavigationContainer>
-    //   <Stack.Navigator initialRouteName="Home"
-    //   screenOptions={{
-    //     headerTransparent: true,
-    //     headerTitleStyle: {color: 'white'},
-    //     headerTintColor: 'white',
-    //   }}>  
-    //     <Stack.Screen name="Home" component={HomeScreen} />
-    //     <Stack.Screen name="Account" component={AccountScreen} />
-    //   </Stack.Navigator>
-      
-    // </NavigationContainer>
-    
+    </NavigationContainer>  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
